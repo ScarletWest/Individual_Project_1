42cf1b8293a40fed6d53090f6feddf06467e0598
user <nana-yaa.owusu@ashesi.edu.gh>
2022-10-01 19:50:56 +0000

    This is my version of individual project 1. It consists of 2 files: individualProject.java and algo.java. My input and output files have been included as well.

---
+++ algo.java
@@ -0,0 +1,163 @@
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.PriorityQueue;
+
+public class algo {
+
+    public static Path UniformCostSearch(String sourceAirportID, String destinationAirportID, ArrayList<String[]> map) {
+        PriorityQueue<Node> openList = new PriorityQueue<>();
+        HashSet<String> closedList = new HashSet<>();
+
+        System.out.println("About to do the search on the problem: ");
+        Node initialNode = new Node(sourceAirportID);
+        openList.add(initialNode);
+
+        while (openList.size() > 0) {
+            Node node = openList.poll();
+
+            if (destinationAirportID.equals(node.getState())) {
+                System.out.println("Yay, found a solution!");
+                return node.path();
+            } else {
+                closedList.add(node.getState());
+                System.out.println("Expanding node: " + node);
+
+                try {
+                    ArrayList<String> neighbouringAirports = new ArrayList<>();
+
+                    for (String[] routeArray: map) {
+                        if (!(routeArray[3] == "" && routeArray[5] == "")) {
+                            if (node.getState().equals(routeArray[3])) {
+                                neighbouringAirports.add(routeArray[5]);
+                            }
+                        }
+                    }
+
+                    for (String airportID: neighbouringAirports) {
+                        Node child = new Node(airportID, node, node.getPathCost() + 1);
+
+                        if (!closedList.contains(child.getState()) && !openList.contains(child)) {
+                            openList.add(child);
+                        } else if (openList.contains(child)) {
+                            int cost = openList.peek().getPathCost();
+                            if (cost > child.getPathCost()) {
+                                openList.poll();
+                                openList.add(child);
+                            }
+                        }
+                    }
+                } catch (NullPointerException ne) {
+                    System.out.println(ne.getMessage());
+                }
+            }
+        }
+        System.out.println("After the rather frustrating search, I couldn't find any valid route.");
+        return null;
+    }
+}
+
+class Node implements Comparable<Node> {
+    private String state;
+    private Node parent;
+    private int pathCost;
+
+    public Node(String state) {
+        this.state = state;
+        this.parent = null;
+        this.pathCost = 0;
+    }
+
+    /**
+     *
+     * @param state the airport Id
+     * @param parent the entire node of the calling object
+     * @param pathCost number of flights taken to get this state
+     */
+    public Node(String state, Node parent, int pathCost) {
+        this.state = state;
+        this.parent = parent;
+        this.pathCost = pathCost;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public int getPathCost() {
+        return pathCost;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "state='" + state + '\'' +
+                ", parent=" + parent +
+                ", pathCost=" + pathCost +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node node = (Node) o;
+        return getState().equals(node.getState());
+    }
+
+    @Override
+    public int compareTo(Node o) {
+//        if (this.getPathCost() > o.getPathCost()) {
+//            return 1;
+//        }
+//        else if (this.getPathCost() < o.getPathCost()) {
+//            return -1;
+//        }
+//        else {
+//            return 0;
+//        }
+        return Double.compare(this.getPathCost(), o.getPathCost());
+    }
+
+    public Path path() {
+        ArrayList<String> actionSequence = new ArrayList<>();
+        actionSequence.add(0, this.state);
+        Integer pathCost = this.getPathCost();
+
+        Node node = this.parent;
+        while (!(node == null)) {
+            actionSequence.add(0, node.getState());
+            node = node.getParent();
+        }
+
+        return new Path(actionSequence, pathCost);
+    }
+}
+
+class Path implements Comparable<Path> {
+    ArrayList<String> actionSequence;
+    int pathCost;
+
+    public Path(ArrayList<String> actionSequence, int pathCost) {
+        this.actionSequence = actionSequence;
+        this.pathCost = pathCost;
+    }
+
+    public ArrayList<String> getActionSequence() {
+        return actionSequence;
+    }
+
+    public int getPathCost() {
+        return pathCost;
+    }
+
+    @Override
+    public int compareTo(Path o) {
+        return Double.compare(this.getPathCost(), o.getPathCost());
+    }
+}
--- individualProject.java
+++ individualProject.java
@@ -1,10 +1,7 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.io.*;
+import java.lang.reflect.Array;
+import java.nio.Buffer;
+import java.util.*;
 
 import static java.util.Arrays.deepToString;
 
@@ -13,19 +10,34 @@ public class individualProject {
     public static ArrayList<String[]>  inputData = new ArrayList<>();
     public static HashMap<String, String[]> airportData = new HashMap<>();
     public static HashMap<String, String[]> airlineData = new HashMap<>();
-    public static void main (String[] args) throws IOException {
+    public static List<String> getAirportIDByCityCountry (String city, String country){
+        List<String> AirportID = new ArrayList<String>();
+        for (String[] array: airportData.values()) {
+            if (array[2].equalsIgnoreCase(city) && array[3].equalsIgnoreCase(country)){
+                AirportID.add(array[0]);
+
+            }
+        }
+        return AirportID;
+    }
+
+
+        public static void main (String[] args) throws IOException {
         String line = "";
         String splitBy = ",";
         BufferedReader input_file = null;
         BufferedReader airport = null;
         BufferedReader airline = null;
         BufferedReader route = null;
+//        BufferedWriter output_file = null;
 
         try {
             route = new BufferedReader(new FileReader("/Users/user/Downloads/ICP_Individual_Project/routes.csv"));
             airline = new BufferedReader(new FileReader("/Users/user/Downloads/ICP_Individual_Project/airlines.csv"));
             airport = new BufferedReader(new FileReader("/Users/user/Downloads/ICP_Individual_Project/airports.csv"));
-            input_file = new BufferedReader(new FileReader("/Users/user/Downloads/Cotonou-Ouagadougo.csv"));
+//            input_file = new BufferedReader(new FileReader("/Users/user/Desktop/Senior Year/Semester 1/ICP/Accra-New York.csv"));
+            input_file = new BufferedReader(new FileReader("/Users/user/Desktop/Senior Year/Semester 1/ICP/Accra-Winnipeg.txt"));
+
 
         } catch (
                 IOException e) {
@@ -39,50 +51,141 @@ public class individualProject {
             routes = line.split(splitBy);
             routesData.add(routes);
 //            System.out.println("Routes[Airline code =" + routes[0] + "Airline ID =" + routes[1] + "Source Airport code =" + routes[2] + "Source Airport ID =" + routes[3] + "Destination Airport ID =" + routes[4] + "Destination Airport Code =" + routes[5] + "Codeshare =" + routes[6] + "Stops =" + routes[7] + "Equipment =" + routes[8] + "]");
-
         }
+        route.close();
+
         String[] airlines = new String[0];
         while ((line = airline.readLine()) != null) {
             airlines = line.split(splitBy);
             airlineData.put(airlines[0],airlines);
 //            System.out.println("Airlines[Airline ID =" + airlines[0] + ", Name = " + airlines[1] + ", Alias =" + airlines[3] + ", IATA code = " + airlines[4] + ", Callsign =" + airlines[5] + ", Country = " + airlines[6] + ", Active =" + airlines[7] + "]");
         }
+        airline.close();
+
 //        for (String[] airlineArray: airlineData.values()) {
 //            System.out.println(deepToString(airlineArray));
 //        }
-
-
 
         String[] airports = new String[0];
         while ((line = airport.readLine()) != null) {
             airports = line.split(splitBy);
             airportData.put(airports[0],airports);
-
 //            System.out.println("Airports[Airports ID =" + airports[0] + ", Name =" + airports[1] + ", City =" + airports[2] + ", Country =" + airports[3] + ",IATA code =" + airports[4] + ", ICAO code =" + airports[5] + ", Latitude =" + airports[6] + ", Longitude =" + airports[7] + ", Altitude =" + airports[8] + ", Timezone =" + airports[9] + ", DST = " + airports[10] + " , TZ database time zone =" + airports[11] + " , Type =" + airports[12] + " , Data source =" + airports[13] + "]");
         }
+        airport.close();
 
         String[] input = new String[0];
         while ((line = input_file.readLine()) != null) {
             input = line.split(splitBy);
             inputData.add(input);
-//            System.out.println("Input[Source City = " + input[0] + ", Source country = " + input[1] + ", Destination city = " + input[2] + ", Destination country =" + input[3] + "]");
+//            System.out.println("Input[Source City = " + input[0] + ", Source country = " + input[1]);
+//            System.out.println(input[0]);
         }
+        input_file.close();
+
 
         String[] sourceCityCountry = inputData.get(0);
         String sourceCity = sourceCityCountry[0].trim();
         String sourceCountry = sourceCityCountry[1].trim();
-        ArrayList<Integer> sourceAirportIDs = getAirportIDByCityCountry(sourceCity, sourceCountry);
-//
+        List<String> sourceAirportIDs = getAirportIDByCityCountry(sourceCity,sourceCountry);
+        System.out.println(sourceAirportIDs);
+//            System.out.println(deepToString(sourceCityCountry).getClass().getSimpleName());
+//        System.out.println(sourceCity);
+//            System.out.println(sourceCountry);
+//            System.out.println(getAirportIDByCityCountry("Accra","Ghana"));
+
+// [246, 248, 300]
+            // [230, 100]
+            // 246, 230
+            // 246, 100
+            // 248. 230 ...
+
+
         String[] destinationCityCountry = inputData.get(1);
         String destinationCity = destinationCityCountry[0].trim();
         String destinationCountry = destinationCityCountry[1].trim();
-        ArrayList<Integer> destinationAirportIDs = getAirportIDByCityCountry(destinationCity, destinationCountry);
-//
+        List<String> destinationAirportIDs = getAirportIDByCityCountry(destinationCity,destinationCountry);
+        System.out.println(destinationAirportIDs);
+
 //        if ((input[0] == airports[2]) && (input[1] == airports[3])) {
 //            System.out.println(airports[2] + "," + airports[3]);
 //
 //        }
 
+        ArrayList<Path> results = new ArrayList<>();
+         for (String sourceAirportID: sourceAirportIDs) {
+             for (String destinationAirportID: destinationAirportIDs) {
+                 Path result = algo.UniformCostSearch(sourceAirportID, destinationAirportID, routesData);
+                 if (result != null) {
+                     results.add(result);
+                 }
+             }
+         }
+//         System.out.println(results);
+
+        Collections.sort(results);
+
+         Path optimalPath = results.get(0);
+         ArrayList<String> actionSequence = optimalPath.getActionSequence();
+         int cost = optimalPath.getPathCost();
+         //System.out.println("Action sequence: " + actionSequence);
+         //System.out.println("Path cost: " + cost);
+
+         StringBuilder data = new StringBuilder();
+         int iterations = 0;
+         int stops = 0;
+
+         for (String airportID: optimalPath.getActionSequence()) {
+             for (String[] routeArray: routesData) {
+                 ArrayList<String[]> possibleRoutes = new ArrayList<>();
+                 if (routeArray[3].equals(airportID)) {
+                     possibleRoutes.add(routeArray);
+                 }
+                 for (String[] possibleRoute: possibleRoutes) {
+                     if (iterations < optimalPath.getActionSequence().size() - 1) {
+                         if (possibleRoute[5].equals(optimalPath.getActionSequence().get(iterations + 1))) {
+                             data.append((iterations + 1) + ". " +
+                                     possibleRoute[0] + " from " +
+                                     airportData.get(possibleRoute[3])[4] + " to " +
+                                     airportData.get(possibleRoute[5])[4] + " " +
+                                     possibleRoute[7] + " stops.\n"
+                             );
+                             iterations++;
+                             stops += Integer.parseInt((possibleRoute[7]));
+                         }
+                     }
+                 }
+             }
+         }
+         data.append("\nTotal flights: ").append(optimalPath.getPathCost());
+         data.append("\nTotal stops: ").append(stops);
+         data.append("\nOptimality criteria: flights");
+
+         try {
+             BufferedWriter output_file = new BufferedWriter((new FileWriter("/Users/user/Desktop/Senior Year/Semester 1/ICP/Accra-Winnipeg-output.txt")));
+             String Data = data.toString();
+             String[] output = Data.split("\n");
+             for (String info : output) {
+                 output_file.write(info);
+                 output_file.newLine();
+             }
+             System.out.println("\nFile output successfully created.");
+             output_file.close();
+         }
+         catch (FileNotFoundException fe) {
+             System.out.println(fe.getMessage());
+         }
+
+
+//         for (Object[] result: results) {
+//             if (result != null) {
+//                 ArrayList<String[]> actionSequence = (ArrayList<String[]>) result[0];
+//                 Integer cost = (int) result[1];
+//                 System.out.println("Action sequence: " + actionSequence);
+//                 System.out.println("Path cost: " + cost);
+//                 System.out.println();
+//             }
+//         }
     }
 }
 
